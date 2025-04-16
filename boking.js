7bdefedb3230359a5b1db89791e2e812b75c7bdf
AT-devloper <ayushap.poddar@gmail.com>
2025-04-16 15:00:33 +0530

    16apl

--- boking.js
+++ boking.js
@@ -1,105 +1,147 @@
 
-let fetchData= async()=>{
 
-    let url="http://localhost:3000/user"
-   
-    let res = await fetch(url,{method:"GET"})
+function bo(){
 
-    let data = await res.json()
+let ddata={
+    name : document.querySelector("#nam").value,
+    cont : document.querySelector("#con").value
+}
 
-    console.log(data);
+localStorage.setItem("Sddata",JSON.stringify(ddata));
+let deta =JSON.parse(localStorage.getItem('Sddata'));
 
-    let dfile=document.querySelector("#datafile")
+if(deta.name==""){ 
 
-    data.map((e)=>{
-        dfile.innerHTML+=`
-        <tr>
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Enter Name",
 
-        <td>${e.name}</td>
-        <td>${e.contact}</td>
-        <td onclick="Del('${e.id}')"> Delete </td>
-        <td onclick="Ffill('${e.id}')"> Edit </td>
-        
-        </tr>
-        
-        `}
-    )}
-
-    let Del=(id)=>{
-            let url=`http://localhost:3000/user/${id}`
-            fetch(url,{method:"DELETE"})
-    }
-
-
-
-    let book=()=>{
-
-        let inpname = document.querySelector('#nam').value
-        let inpcont = document.querySelector("#con").value
-      
-        
-      
-        let url="http://localhost:3000/user"
-
-        fetch(url ,{
-            method :"POST",
-            headers :{
-                "Content-Type":"appliction/json"
-            },
-            body : JSON.stringify({
-                    "name": inpname ,
-                    "contact": inpcont,
-                    
-            })
-        })
-
-        location.href="show.html"
-        return false
-    }
-
-    let Ffill=async(id)=>{
-
-      let url=`http://localhost:3000/user/${id}`
-      let response=await fetch(url,{method:"GET"})
-      let data=await response.json()
-
-      console.log(data);
-
-      let Fmdata=document.querySelector("#showdata")
-      Fmdata.innerHTML=` 
-      
-      Name : <input type="text" id="upname" value="${data.name}"> <br><br>
-     
-      Contact : <input type="text" id="upcont" value="${data.contact}"> <br><br>
-     
-      
-      <input type="submit" value="Edited" onclick="return Fupdate('${data.id}')">
+    });
     
-      `
+    return false;
+}
 
-  }
-  
-let Fupdate =(id)=>{
-      let inpname = document.querySelector('#upname').value
+else if(deta.cont==""){
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Enter contact number",
+        });
+    // alert("Enter drop location")
     
-      let inpcont = document.querySelector("#upcont").value
-     
-      
-      let url=`http://localhost:3000/user/${id}`
+    return false;
+}
 
-      fetch(url ,{
-          method :"PUT",
-          headers :{
-              "Content-Type":"appliction/json"
-          },
-          body : JSON.stringify({
-                  "name": inpname ,
-               
-                  "contact": inpcont,
-               
-              })
-      })
+else if(isNaN(deta.cont)){
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Enter Contact in number",
+    
+        });
+    
+    return false 
+}
+
+else if(deta.cont.length>10 || deta.cont.length<10){
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Enter 10 digit number",
+    
+        });
+    return false 
+}
+
+    alert("done")
 }
 
 
+let fetchData=async()=>{
 
+let url="http://localhost:3000/user"
+let respo=await fetch(url,{method:"GET"})
+let data=await respo.json()
+console.log(data);
+
+
+let fil=document.querySelector("#datafile")
+
+data.map((e)=>{
+fil.innerHTML+=`
+
+<tr>
+<td>${e.name}</td>
+<td>${e.contact}</td>
+<td onclick="Del('${e.id}')">Del</td>
+<td onclick="Fifi('${e.id}')">Edit</td>
+
+</tr>
+
+`
+})}
+
+let Del=(id)=>{
+
+let url=`http://localhost:3000/user/${id}`
+fetch(url,{method:"DELETE"})
+}
+
+let ok=()=>{
+let inpname = document.querySelector('#nam').value
+let inpcont = document.querySelector('#con').value
+
+let url="http://localhost:3000/user"
+
+fetch(url,{
+    method:"POST",
+    headers :{
+        "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+        "name":inpname,
+        "contact":inpcont,
+
+    })
+})
+
+location.href="show.html"
+return false
+}
+
+let Fifi=async(id)=>{
+let url=`http://localhost:3000/user/${id}`
+let response=await fetch(url,{methood:"GET"})
+let data = await response.json()
+
+console.log(data);
+
+let fomdt=document.querySelector("#showdata")
+fomdt.innerHTML=`
+<br><br>
+Name : <input type="text" id="upname" value="${data.name}"> Contact : <input type="text" id="upcont" value="${data.contact}"> <br><br>
+<input type="submit" id="bt" value="Edited" onclick="return Fup('${data.id}')">
+
+
+`
+}
+
+let Fup=(id)=>{
+let inpname = document.querySelector('#upname').value
+let inpcont = document.querySelector('#upcont').value
+
+let url=`http://localhost:3000/user/${id}`
+
+fetch(url,{
+    method:"PUT",
+    headers :{
+        "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+        "name":inpname,
+        "Contact":inpcont,
+
+    })
+});
+};
